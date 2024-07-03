cbca3e69f9f80d6658bb0cc087c1f20425d37e4c
Arun3622 <97716134+Arun3622@users.noreply.github.com>
2024-07-03 23:40:13 +0530

    update

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -1,12 +1,37 @@
-import React from 'react';
-import { AppBar, Toolbar, Button, Box, Typography } from '@mui/material';
-import Link from 'next/link';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
 import { styled } from '@mui/system';
+import Link from 'next/link';
+
+const pages = [
+  { name: 'Bill Detail', path: '/' },
+  { name: 'Update', path: '/update' },
+  { name: 'Bill', path: '/bill' },
+  { name: 'Live', path: '/live' },
+];
+const settings = [
+  { name: 'New User', path: '/Newuser' },
+  { name: 'Account', path: '/account' },
+  { name: 'Logout', path: '/logout' },
+  { name: 'Login', path: '/login' }
+];
 
 const NavbarButton = styled(Button)({
   color: '#ffffff',
   textTransform: 'none',
-  padding: '0 20px', // Add some padding for spacing
+  padding: '0 20px',
   '&:hover': {
     backgroundColor: 'transparent',
   },
@@ -15,51 +40,154 @@ const NavbarButton = styled(Button)({
 const NavbarLink = styled(Link)({
   textDecoration: 'none',
   color: 'inherit',
-  padding: '0 5px', // Increase padding for better spacing
+  padding: '0 5px',
 });
 
-const Navbar = () => {
+function ResponsiveAppBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
-    <AppBar position="static" sx={{ background: '#333' }}>
-      <Toolbar sx={{
-              display: 'flex',
-              justifyContent: 'center',
-            }}>
-        <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}> {/* Add gap for spacing between buttons */}
-          <NavbarButton>
-            <NavbarLink href="/Newuser">
-              <Typography variant="h6" component="span">New User</Typography> {/* Use variant="h6" for larger text */}
-            </NavbarLink>
-          </NavbarButton>
-          <NavbarButton>
-            <NavbarLink href="/">
-              <Typography variant="h6" component="span">Bill Detail</Typography>
-            </NavbarLink>
-          </NavbarButton>
-          <NavbarButton>
-            <NavbarLink href="/update">
-              <Typography variant="h6" component="span">Update</Typography>
-            </NavbarLink>
-          </NavbarButton>
-          <NavbarButton>
-            <NavbarLink href="/bill">
-              <Typography variant="h6" component="span">Bill</Typography>
-            </NavbarLink>
-          </NavbarButton>
-          <NavbarButton>
-            <NavbarLink href="/live">
-              <Typography variant="h6" component="span">Live</Typography>
-            </NavbarLink>
-          </NavbarButton>
-          <NavbarButton>
-            <NavbarLink href="/login">
-              <Typography variant="h6" component="span">Login</Typography>
-            </NavbarLink>
-          </NavbarButton>
-        </Box>
-      </Toolbar>
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            logo
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page.name} onClick={handleCloseNavMenu}>
+                  <NavbarLink href={page.path}>
+                    <Typography textAlign="center">{page.name}</Typography>
+                  </NavbarLink>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#"
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LOGO
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'center' }}>
+            {pages.map((page) => (
+              <NavbarButton key={page.name}>
+                <NavbarLink href={page.path}>
+                  <Typography variant="h6" component="span">{page.name}</Typography>
+                </NavbarLink>
+              </NavbarButton>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting.name} onClick={handleCloseUserMenu}>
+                  <NavbarLink href={setting.path}>
+                    <Typography textAlign="center">{setting.name}</Typography>
+                  </NavbarLink>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
     </AppBar>
   );
-};
+}
 
-export default Navbar;
+export default ResponsiveAppBar;
